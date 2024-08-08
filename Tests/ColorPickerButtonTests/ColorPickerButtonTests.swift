20f318c63c962a540b55d0786682e8981f20344f
sake <sake.std.co@gmail.com>
2024-08-09 01:40:31 +0900

    test: Add unit test

--- Tests/ColorPickerButtonTests/ColorPickerButtonTests.swift
+++ Tests/ColorPickerButtonTests/ColorPickerButtonTests.swift
@@ -1,12 +1,45 @@
-import XCTest
 @testable import ColorPickerButton
+import SwiftUI
+import ViewInspector
+import XCTest
+
+// MARK: - Inspectable
+
+extension ColorPickerButton: Inspectable {}
+extension ColorPickerButton.Color: Inspectable {}
+
+// MARK: - Tests
 
 final class ColorPickerButtonTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    /// Test the button image system name
+    func testButtonImageSystemName() throws {
+        let colors: [ColorPickerButton.Color] = [
+            .init(id: 1, color: .black)
+        ]
+        let sut = ColorPickerButton(colors: colors, onSelected: { _ in })
+        let inspection = try sut.inspect()
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        let button = try inspection.find(viewWithId: "colorButton")
+        let image = try button.find(ViewType.Image.self)
+        let systemName = try image.actualImage().name()
+
+        XCTAssertEqual(systemName, "paintpalette")
+    }
+
+    /// Test the button image color
+    func testInitiallySelectedColor() throws {
+        let colors: [ColorPickerButton.Color] = [
+            .init(id: 1, color: .red),
+            .init(id: 2, color: .green),
+            .init(id: 3, color: .blue)
+        ]
+        let sut = ColorPickerButton(colors: colors, onSelected: { _ in })
+        let inspection = try sut.inspect()
+
+        let button = try inspection.find(viewWithId: "colorButton")
+        let image = try button.find(ViewType.Image.self)
+        let color = try image.foregroundColor()
+
+        XCTAssertEqual(color, Color.red)
     }
 }
